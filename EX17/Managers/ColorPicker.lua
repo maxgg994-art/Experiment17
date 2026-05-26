47a487ea15a2ba560f0aa5f9e40ac5e5f64b3104
maxgg994-art <maxgg994@gmail.com>
2026-05-26 19:45:24 +0400

    ColorPicker.lua

--- EX17/Managers/ColorPicker.lua
+++ EX17/Managers/ColorPicker.lua
@@ -2,7 +2,7 @@
 -- HSV Color Picker с HEX-вводом, открывается при клике на превью цвета
 
 local ColorPicker = {}
-local Services = require(script.Parent.Parent.Core.Services)
+local Services = _G.Experiment17.Services
 local Utils = require(script.Parent.Parent.Core.Utils)
 local State = require(script.Parent.Parent.Core.State)
 local GUIManager = require(script.Parent.GUIManager)
